987b33a016b439924b658523daf0f2421a863aa0
Ross Morgan <morgan.ross.512@gmail.com>
2022-12-28 23:42:58 +0000

    fix attribute parsing

--- src/sections/attr.rs
+++ src/sections/attr.rs
@@ -7,6 +7,11 @@ fn is_ident(s: &str) -> bool {
 }
 
 fn is_literal(s: &str) -> bool {
+    match s {
+        "true" | "false" => { return true; }
+        _ => {}
+    }
+
     if s.starts_with('"') && s.ends_with('"') && {
         // If char has only 2 quotes
         s.chars().fold(0, |mut acc, c| { if c == '"' { acc += 1; } acc }) == 2
@@ -29,23 +34,24 @@ pub fn parse_attr(attr: &str) -> Attr {
 
     let attr_data = chars.as_str().to_string();
 
-    if attr_data.contains("=") {
+    if !attr_data.contains('=') {
+        name = attr_data.clone();
+    }
+    else
+    if attr_data.contains('=') {
         let sides = attr_data.split("=")
             .map(str::trim)
             .map(str::to_string)
-            .take(2)
             .collect::<Vec<_>>();
 
-            let lhs = sides.get(0).unwrap().to_string();
+            name = sides.get(0).unwrap().to_string();
             let rhs = sides.get(1).unwrap().to_string();
 
-            value = Some(Token::ATTR(lhs, Some(Box::new(Token::STR(rhs)))));
+            value = Some(Token::STR(rhs));
     }
 
     Attr { name, value }
 }
-
-
 
 pub trait ValidAttr {
     fn is_attr(&self) -> bool;
